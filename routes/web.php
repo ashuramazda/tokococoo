27642921afcf844817efd266e1486a715c4f830f
ashuramazda <rickyariadi@esrapdigital.com>
2019-07-20 01:31:15 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ Route::get('install/pre-installation', 'InstallController@preInstallation');
 Route::get('install/configuration', 'InstallController@getConfiguration');
 Route::post('install/configuration', 'InstallController@postConfiguration');
 Route::get('install/complete', 'InstallController@complete');
+Route::get('/foo', function () {
+Artisan::call('storage:link');
+});
